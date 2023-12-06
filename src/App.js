9dc2489573821f6ad6882095bdb06c978a6342c7
pilips719 <andery.moshis@gmail.com>
2023-12-06 09:02:19 -0500

    app.js changed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import './App.css';
 
 function App() {
-  const [person, setPerson] = useState({
+  const [person, setPerson] = useState({ 
     firstName: "",
     lastName: "",
     mission: "",
